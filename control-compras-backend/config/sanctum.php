ab4f38e09c92674cc0124c2c38515b424dca28a1
User <user@cooperativa.com>
2026-07-30 12:04:08 -0400

    Corregir configuracion Sanctum authenticate_session y dominios stateful para prevenir Unauthenticated

--- control-compras-backend/config/sanctum.php
+++ control-compras-backend/config/sanctum.php
@@ -18,12 +18,13 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
-    ))),
+    'stateful' => array_filter(array_merge(
+        explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,localhost:5173,localhost:8000,127.0.0.1,127.0.0.1:5173,127.0.0.1:8000,::1')),
+        [
+            Sanctum::currentApplicationUrlWithPort(),
+            isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : null,
+        ]
+    )),
 
     /*
     |--------------------------------------------------------------------------
@@ -79,7 +80,7 @@ return [
     */
 
     'middleware' => [
-        'authenticate_session' => AuthenticateSession::class,
+        'authenticate_session' => null,
         'encrypt_cookies' => EncryptCookies::class,
         'validate_csrf_token' => ValidateCsrfToken::class,
     ],
